ab7eed343c36d2363b1bbc9a59f772f8475aba46
dzale <ndzakovic@yahoo.com>
2017-10-19 03:01:56 +0200

    Added ApiTransformer and changed path to lang folder

---
+++ src/Transformers/ApiTransformer.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ApiTransformer
+ *
+ * @author dzale
+ */
+
+namespace Dinkara\DinkoApi\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
+
+abstract class ApiTransformer extends TransformerAbstract{
+        
+    //protected abstract function transformFromModel($data);
+    
+    protected function item($data, $transformer, $resourceKey = null)
+    {
+        if($data == null){
+            return $this->null();
+        }
+        
+        return new Item($data, $transformer, $resourceKey);
+    }
+    
+    protected function collection($data, $transformer, $resourceKey = null)
+    {
+        if($data == null){
+            return $this->null();
+        }
+                
+        return new Collection($data, $transformer, $resourceKey);
+    }     
+    
+    protected function transformFromModel($item){
+        
+        $fillable = $item->getDispalyable();
+        
+        foreach ($fillable as $value) {
+            $arr[$value] = eval('return $item->'.$value.';');
+        }
+        return $arr;
+    }
+    
+}
