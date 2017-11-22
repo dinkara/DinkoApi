9ef660e7f34bbcee6fd24d65a497546490815879
dzale <ndzakovic@yahoo.com>
2017-11-22 02:35:08 +0100

    Fixed comments and exluded functions form resourece controller which are not neccessary

--- src/Http/Controllers/ResourceController.php
+++ src/Http/Controllers/ResourceController.php
@@ -16,22 +16,11 @@ class ResourceController extends ApiController
         $this->repo = $repo;
         $this->transformer = $transformer;
     }
-    /**
-     * Display a listing of the item.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {   
-        try{
-            return ApiResponse::Collection($this->repo->all(), new $this->transformer);
-        } catch (QueryException $e) {
-            return ApiResponse::InternalError($e->getMessage());
-        } 
-    }
     
     /**
-     * Display a listing of the item.
+     * Paginated items
+     * 
+     * Display a list of paginated items .
      *
      * @return \Illuminate\Http\Response
      */
@@ -45,16 +34,8 @@ class ResourceController extends ApiController
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return ApiResponse::NotFound();
-    }
-
-    /**
+     * Get Single Item
+     * 
      * Display the specified item.
      *
      * @param  int  $id
@@ -85,7 +66,9 @@ class ResourceController extends ApiController
 
     
     /**
-     * Store a newly created resource in storage.
+     * Create item
+     * 
+     * Store a newly created item in storage.
      *
      * @param  $data
      * @return \Illuminate\Http\Response
@@ -100,6 +83,8 @@ class ResourceController extends ApiController
     }
 
     /**
+     * Update item
+     * 
      * Update the specified resource in storage.
      *
      * @param  $data
@@ -117,25 +102,10 @@ class ResourceController extends ApiController
         }
         return ApiResponse::ItemNotFound($this->repo->getModel());    
     }
-    /**
-     * Show the item for editing it.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {  
-        try{
-            if($item = $this->repo->find($id)){
-                return ApiResponse::Item($item->getModel(), new $this->transformer);
-            }
-        } catch (QueryException $e) {
-            return ApiResponse::InternalError($e->getMessage());
-        }  
-        return ApiResponse::ItemNotFound($this->repo->getModel());
-    }
 
     /**
+     * Remove item
+     * 
      * Remove the specified item from storage.
      *
      * @param  int  $id
